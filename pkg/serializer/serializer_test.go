6babadf7d4e383f9ac16a24f638f7584fd682e5c
Rrooach <731297911@qq.com>
2020-04-22 00:39:46 +0800

    add hypercall, bugs remain

---
+++ pkg/serializer/serializer_test.go
@@ -0,0 +1,46 @@
+// Copyright 2017 syzkaller project authors. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package serializer
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestSerializer(t *testing.T) {
+	x := &X{
+		Y: Y{1},
+		P: &Y{2},
+		A: []Y{{3}, {4}},
+		B: true,
+		S: "a\x09b",
+		T: T1,
+		F: nil,
+	}
+	buf := new(bytes.Buffer)
+	Write(buf, x)
+	t.Logf("\n%s", buf.String())
+	t.Logf("\n%#v", x)
+}
+
+type X struct {
+	Y Y
+	P *Y
+	A []Y
+	B bool
+	S string
+	T T
+	F func()
+}
+
+type Y struct {
+	V int
+}
+
+type T int
+
+const (
+	_ T = iota
+	T1
+)
